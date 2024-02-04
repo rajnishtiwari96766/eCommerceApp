2b3a6dd264da9ec907caf7fe54b0af7ce00a8faa
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2024-02-04 16:41:49 +0530

    new commit

--- src/Pages/main_page/cart_qty.jsx
+++ src/Pages/main_page/cart_qty.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Cart_qty = ({amount,inc_qty,dec_qty}) => {
   return (
     <div>
-        <button onClick={()=>dec_qty}><i class="fa-solid fa-minus"></i></button>
+        <button onClick={()=>dec_qty()}><i class="fa-solid fa-minus"></i></button>
         <p>{amount}</p>
-        <button onClick={()=>inc_qty}><i class="fa-solid fa-plus"></i></button>
+        <button onClick={()=>inc_qty()}><i class="fa-solid fa-plus"></i></button>
     </div>
   )
 }
