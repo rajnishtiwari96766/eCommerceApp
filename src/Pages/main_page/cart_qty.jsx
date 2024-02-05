3b1beb360919ca729f6ca4a49e4da741ec507bfc
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2024-02-05 16:34:49 +0530

    new commit

--- src/Pages/main_page/cart_qty.jsx
+++ src/Pages/main_page/cart_qty.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Cart_qty = ({amount,inc_qty,dec_qty}) => {
   return (
@@ -6,6 +7,9 @@ const Cart_qty = ({amount,inc_qty,dec_qty}) => {
         <button onClick={()=>dec_qty()}><i class="fa-solid fa-minus"></i></button>
         <p>{amount}</p>
         <button onClick={()=>inc_qty()}><i class="fa-solid fa-plus"></i></button>
+        <NavLink to='/cart'>
+        <button className='p-2 bg-slate-200'>Go to cart</button>
+        </NavLink>
     </div>
   )
 }
