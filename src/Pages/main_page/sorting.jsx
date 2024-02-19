486e0f063c970c2d6ae194614a9b634a5348f826
Rajnish Tiwari <rajnishtiwari96766@gmail.com>
2024-02-19 08:48:56 +0530

    new commit

--- src/Pages/main_page/sorting.jsx
+++ src/Pages/main_page/sorting.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import { customHook2 } from '../../context/filter_context'
 
 const Sorting = () => {
-    const{gridView,listView}=customHook2()
+    const{gridView,listView,filter_products}=customHook2()
   return (
-    <div className=''>
+    <div className='flex justify-between p-2'>
+        <div>
         <button className='p-2' onClick={gridView}><i class="fa-solid fa-grip"></i></button>
-
         <button onClick={listView}><i class="fa-solid fa-list"></i></button>
+        </div>
+
+        <div>{filter_products.length} Products Available</div>
+
+        <div>Categorise</div>
     </div>
   )
 }
